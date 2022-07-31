6f306c0261b2bd7aa9c4a9bde3548310d3e1f44d
BiomimicryCafe <roan.things@gmail.com>
2022-07-31 10:23:30 -0500

    Update Python-Bluetooth-test.py

--- Python-Bluetooth-test.py
+++ Python-Bluetooth-test.py
@@ -10,16 +10,16 @@ class CommsService(Service):
       # Base 16 service UUID, This should be a primary service.
       super().__init__("180D", True)
 
-   @TxCharacteristic("2A37", CharFlags.READ)
-   def on_read(self, options):
+   @Characteristic("2A37", CharFlags.READ)
+   def TxCharacteristic(self, options):
       # Characteristics need to return bytes.
       pass
    def send_data(self, new_data):
       # Note that notification is asynchronous (you must await something at some point after calling this).
-      self.on_read.changed(bytes(new_data, "utf-8"))
+      self.TxCharacteristic.changed(bytes(new_data, "utf-8"))
 
-   @RxCharacteristic("2A38", CharFlags.WRITE)
-   def on_data_recieved(self, options):
+   @Characteristic("2A38", CharFlags.WRITE)
+   def RxCharacteristic(self, options):
       # This function is called when the characteristic is read.
       # Since this characteristic is notify only this function is a placeholder.
       # You don't need this function Python 3.9+ (See PEP 614).
